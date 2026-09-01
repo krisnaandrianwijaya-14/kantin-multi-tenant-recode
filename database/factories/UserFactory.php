0f044c005cc613473ce5d6138f150d221d59c339
𝕶𝖗𝖗𝖊𝖊𝖘𝖘𝖓𝖆𝖆𝟎𝟑_ <krisnaandrianwijaya@gmail.com>
2026-09-01 12:42:57 +0700

    fix: add missing return statement in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -46,11 +46,13 @@ class UserFactory extends Factory
     /**
      * Indicate that the model has two-factor authentication configured.
      */
-public function withTwoFactor(): static
-{
-    return $this->state(fn (array $attributes) => [
-        'two_factor_secret' => null,
-        'two_factor_recovery_codes' => null,
-        'two_factor_confirmed_at' => null,
-    ]);
+    public function withTwoFactor(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'two_factor_confirmed_at' => null,
+        ]);
+    }
 }
+
